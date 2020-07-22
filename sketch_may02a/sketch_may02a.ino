4271c0f21771f7fa877a1d98852404ae953e788c
CodeImm <45065137+CodeImm@users.noreply.github.com>
2020-07-22 14:36:22 +0700

    AsciiMassage was added to the uno scetch, all sorts of rubbish in code were removed also

--- sketch_may02a/sketch_may02a.ino
+++ sketch_may02a/sketch_may02a.ino
@@ -1,42 +1,43 @@
-int microphonePin = A0;
+#include <AsciiMassagePacker.h>
+#include <AsciiMassageParser.h>
+
+AsciiMassageParser inbound;
+AsciiMassagePacker outbound;
+
+const int microphonePin = A0;
 const int SoundPin = 9;
-int delaySound = 1000;
-int microphoneValue = 0;
-int chr;
-int val;
-String inString = ""; 
-int frequency=5000;
+long microphoneValue = 0;
+int frequency = 5000;
 
 void setup() {
-  //delay(delaySound);
-  //noTone(SoundPin);
   Serial.begin(9600);
 }
 
 void loop() {
-  if (Serial.available()) {        //если есть принятый символ,
-    //chr = Serial.read();    //  то читаем его
-    inString = Serial.readString();
-    val = inString.toInt();
-//    Serial.println(val);
-    if (val>32 && val<10000){
-      frequency=val;
-//    }
-//    if (val == 30) { //проверяем его на нужную команду
-      //инициализируем переменные для определения размаха
-      int mn = 1024;
-      int mx = 0;
-      tone(SoundPin, frequency); //запускаем генератор звука
-      for (int i = 0; i < 1000; ++i) { //ищем макс и мин значения амплитуды
-        int val = analogRead(microphonePin);
-        mn = min(mn, val);
-        mx = max(mx, val);
+  if ( inbound.parseStream( &Serial ) ) {
+    // parse completed massage elements here.
+    // Does the massage's address match "value"?
+    if ( inbound.fullMatch ("frequency") ) {
+      frequency = inbound.nextInt();
+      if (frequency > 32 && frequency < 10000) {
+        //инициализируем переменные для определения размаха
+        int mn = 1024;
+        int mx = 0;
+        tone(SoundPin, frequency); //запускаем генератор звука
+        for (int i = 0; i < 1000; ++i) { //ищем макс и мин значения амплитуды
+          int val = analogRead(microphonePin);
+          mn = min(mn, val);
+          mx = max(mx, val);
+        }
+        //нашли размах амплитуды
+        microphoneValue = mx - mn;
+
+        outbound.beginPacket("value"); // Start a packet with the address called "value".
+        outbound.addLong(microphoneValue); // Add a reading of analog 0.
+        outbound.streamPacket(&Serial); // End the packet and stream it.
+
+        noTone(SoundPin);
       }
-      //нашли размах амплитуды
-      microphoneValue = mx - mn;
-      Serial.println(microphoneValue, DEC);
-      delay(3000);
-      noTone(SoundPin);
     }
   }
 }
